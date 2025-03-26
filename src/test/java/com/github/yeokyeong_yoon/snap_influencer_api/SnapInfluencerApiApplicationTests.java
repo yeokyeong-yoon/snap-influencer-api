68891e5ef0b3d8c492053e9f9c0fe38b2bd435ab
yeokyeong-yoon <yeokyeongy@gmail.com>
2025-03-26 22:16:21 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/com/github/yeokyeong_yoon/snap_influencer_api/SnapInfluencerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yeokyeong_yoon.snap_influencer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapInfluencerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
