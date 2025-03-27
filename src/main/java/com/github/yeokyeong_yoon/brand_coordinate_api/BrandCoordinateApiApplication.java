fb4833de92c925d8df089b0c00de0a7335af2695
yeokyeong-yoon <yeokyeongy@gmail.com>
2025-03-27 12:53:58 +0900

    chore: Update build configuration and main application class

--- src/main/java/com/github/yeokyeong_yoon/brand_coordinate_api/BrandCoordinateApiApplication.java
+++ src/main/java/com/github/yeokyeong_yoon/brand_coordinate_api/BrandCoordinateApiApplication.java
@@ -2,14 +2,21 @@ package com.github.yeokyeong_yoon.brand_coordinate_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import java.util.Map;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaRepositories
+@RestController
 public class BrandCoordinateApiApplication {
 
     public static void main(String[] args) {
+        System.out.println("Starting application...");
+        System.setProperty("logging.level.org.springframework", "DEBUG");
         SpringApplication.run(BrandCoordinateApiApplication.class, args);
+        System.out.println("Application started!");
     }
-
 } 
